6bc17816436b249bfbf8eea7f72ab981f3eae04f
jonathanahern <modestfool@gmail.com>
2020-06-08 22:57:10 -0400

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  mount ShopifyApp::Engine, at: '/'
+
+  root to: 'api/v1/home#index'
+  namespace :api do
+    namespace :v1 do
+      get 'home/index'
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
